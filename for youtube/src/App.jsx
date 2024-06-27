2d5946e44271d2aff9bc4a361701abb47be9e3ae
Kavindu Maduranga <144511926+gmandroroshe@users.noreply.github.com>
2024-06-27 16:26:20 +0530

    add style for full site

--- for youtube/src/App.jsx
+++ for youtube/src/App.jsx
@@ -1,7 +1,7 @@
 // import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 // import Header from './header1.jsx';
-// import Navbar from './Navbar1.jsx';
+import Navbar from './Navbar1.jsx';
 // import Footer from './Footer.jsx';
 // import Food from './Food.jsx';
 import Card from './Card.jsx';
@@ -14,14 +14,37 @@ import Card1 from '/Images/card.jpg'
 function App() {
   return (
     <>
-      {/* <Navbar/>
-      <Header/>
+      <Navbar/>
+      {/* <Header/>
       <Food/>
       <Footer/> */}
       {/* Other components */}
+      <br />
       <div className="card-container">
         <Card p1="lorem 1" Card1={Card1} />
         <Card p1="lorem 2" Card1={Card1} />
+        <Card p1="lorem 3" Card1={Card1} />
+        <Card p1="lorem 3" Card1={Card1} />
+      </div>
+      <br />
+      <div className="card-container">
+        <Card p1="lorem 1" Card1={Card1} />
+        <Card p1="lorem 2" Card1={Card1} />
+        <Card p1="lorem 3" Card1={Card1} />
+        <Card p1="lorem 3" Card1={Card1} />
+      </div>
+      <br />
+      <div className="card-container">
+        <Card p1="lorem 1" Card1={Card1} />
+        <Card p1="lorem 2" Card1={Card1} />
+        <Card p1="lorem 3" Card1={Card1} />
+        <Card p1="lorem 3" Card1={Card1} />
+      </div>
+      <br />
+      <div className="card-container">
+        <Card p1="lorem 1" Card1={Card1} />
+        <Card p1="lorem 2" Card1={Card1} />
+        <Card p1="lorem 3" Card1={Card1} />
         <Card p1="lorem 3" Card1={Card1} />
       </div>
       {/* <Condition isLoggedIn={true} username="Kavindu" />
--- for youtube/src/Navbar1.jsx
+++ for youtube/src/Navbar1.jsx
@@ -1,8 +1,16 @@
 function Navbar (){
     return (
-        <nav>
-            <h1>My website</h1>
-        </nav>
+    <nav className="navbar">
+      <div className="navbar-logo">
+        <h1>My Website</h1>
+      </div>
+      <ul className="navbar-links">
+        <li><a href="#home">Home</a></li>
+        <li><a href="#about">About</a></li>
+        <li><a href="#services">Services</a></li>
+        <li><a href="#contact">Contact</a></li>
+      </ul>
+    </nav>
     );
 }
 
